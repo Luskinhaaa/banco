692069fa4ee74182e601d344cbfbdefece8a43ee
Luskinhaaa <134635596+Luskinhaaa@users.noreply.github.com>
2024-10-03 18:48:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -115,7 +115,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 if (sender.balance >= amount) {
                     if (amount > 2000) {
-                        const confirmTransfer = confirm(`Deseja mesmo transferir R$ ${amount} de ${fromPlayer} para ${toPlayer}. Quer continuar?`);
+                        const confirmTransfer = confirm(`PROCEDIMENTO DE SEGURANÇA` + \n + ` Deseja mesmo transferir R$ ${amount} de ${fromPlayer} para ${toPlayer}. Quer continuar?`);
                         if (!confirmTransfer) return; // Cancela a transferência se o usuário não confirmar
                     }
 
